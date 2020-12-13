a64506a657509235b0f6b49ff515a290d6522b99
jackou <oujie_forwork@foxmail.com>
2020-12-13 21:54:57 +0800

    添加gradle工程模板

---
+++ login/src/main/java/com/gacrnd/gcs/login/debug/MainActivityDebug.java
@@ -0,0 +1,16 @@
+package com.gacrnd.gcs.login.debug;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gacrnd.gcs.login.R;
+
+public class MainActivityDebug extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_debug);
+    }
+}
